0be98812965ec0dfd190d0bd30d351c188254585
Sunghee Lee <31603479+Sunghee2@users.noreply.github.com>
2019-01-08 23:59:39 +0900

    10991 - 별 찍기 - 16 :book:

---
+++ 10991/src/Main.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        for(int i = 1; i <= n; i++) {
+            for(int j = 0; j < n - i; j++) {
+                bw.write(" ");
+            }
+            for(int j = 0; j < i; j++) {
+                bw.write("* ");
+            }
+            bw.write("\n");
+        }
+
+        bw.close();
+        br.close();
+    }
+}
