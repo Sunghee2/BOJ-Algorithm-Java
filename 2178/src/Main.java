87159f460e0f5ea94e350e3ebee859e591c67309
Sunghee Lee <31603479+Sunghee2@users.noreply.github.com>
2019-01-11 15:13:32 +0900

    2178 - 미로 탐색 :book:
    확인 위한 출력문 삭제

--- 2178/src/Main.java
+++ 2178/src/Main.java
@@ -61,13 +61,6 @@ public class Main {
 
         bfs(0, 0);
 
-        for(int i = 0; i < n ; i++) {
-            for(int j = 0; j < m; j++) {
-                System.out.print(distance[i][j]);
-            }
-            System.out.print("\n");
-        }
-
         bw.write(String.valueOf(distance[n - 1][m - 1]));
 
         bw.close();
