68b9128eb69249d210b3f3c8cca5e993e3b085dc
Sunghee Lee <31603479+Sunghee2@users.noreply.github.com>
2019-01-15 09:28:16 +0900

    10950 - A+B-3 :book:

---
+++ 10950/src/Main.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int testcase = Integer.parseInt(br.readLine());
+        while(testcase-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+
+            bw.write(String.valueOf(x + y + "\n"));
+        }
+
+        bw.close();
+        br.close();
+    }
+}
